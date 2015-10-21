afd8724d4b89bfa8e884b6aa5faadfc28306d644
xzdesk <418250505@qq.com>
2015-10-22 00:39:50 +0800

    删除安装文件

---
+++ caiji/config/config.php
@@ -0,0 +1,40 @@
+<?php
+return array(
+    'logs'=>array(
+        'path'=>'backup/logs/log',
+        'type'=>'file'
+    ),
+    'DB'=>array(
+        'type'=>'mysqli',
+        'tablePre'=>'iwebshop_',
+        'read'=>array(
+            array('host'=>'192.168.1.208:3306','user'=>'root','passwd'=>'','name'=>'iwebshop'),
+        ),
+
+        'write'=>array(
+            'host'=>'192.168.1.208:3306','user'=>'root','passwd'=>'','name'=>'iwebshop',
+        ),
+    ),
+    'interceptor' => array(
+        'themeroute@onCreateController',
+        'layoutroute@onCreateView',
+        'hookCreateAction@onCreateAction',
+        'hookFinishAction@onFinishAction'
+    ),
+    'langPath' => 'language',
+    'viewPath' => 'views',
+    'skinPath' => 'skin',
+    'classes' => 'classes.*',
+    'rewriteRule' =>'url',
+    'theme' => array('pc' => 'default','mobile' => 'mobile'),
+    'skin' => array('pc' => 'default','mobile' => 'default'),
+    'timezone'	=> 'Etc/GMT-8',
+    'upload' => 'upload',
+    'dbbackup' => 'backup/database',
+    'safe' => 'cookie',
+    'lang' => 'zh_sc',
+    'debug'=> false,
+    'configExt'=> array('site_config'=>'config/site_config.php'),
+    'encryptKey'=>'302a815e4748daa488a0c568cecb4d73',
+);
+?>
--- caiji/index.php
+++ caiji/index.php
@@ -1,6 +1,7 @@
 <?php
+define('START_TIME', microtime(true));
 $iweb = dirname(__FILE__)."/lib/iweb.php";
 $config = dirname(__FILE__)."/config/config.php";
 require($iweb);
-IWeb::createWebApp($config)->run();
+$iwebApp = IWeb::createWebApp($config)->run();
 ?>
